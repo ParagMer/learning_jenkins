d583b27f244a3cbc172dba01f32968bf87704bb4
Parag Mer <paragmerpm49@gmail.com>
2024-08-03 22:21:26 +0530

    first commit

---
+++ envsetup.sh
@@ -0,0 +1,22 @@
+if [-d "env"]
+then
+    echo "Python virtual env exists"
+else
+    python3 -m venv env
+fi
+
+echo $pwd
+source env/bin/activate
+
+pip3 install -r requirements.txt
+
+if [-d "logs"]
+then
+    echo "Log folder exists"
+else
+    mkdir logs
+    touch logs/error.log logs.access.log
+fi
+
+sudo chmod -R 777 logs
+echo "envsetup finishes"
