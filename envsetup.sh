2abc5d1033af9c9e80061e273148978b0ec8294f
Parag Mer <paragmerpm49@gmail.com>
2024-08-03 22:52:22 +0530

    modified envsetup for python issue

--- envsetup.sh
+++ envsetup.sh
@@ -1,22 +1,30 @@
-if [-d "env"]
-then
+#!/bin/bash
+
+# Check if the virtual environment directory exists
+if [ -d "env" ]; then
     echo "Python virtual env exists"
 else
     python3 -m venv env
 fi
 
-echo $pwd
+# Print the current directory
+echo $PWD
+
+# Activate the virtual environment
 source env/bin/activate
 
-pip3 install -r requirements.txt
+# Install the required Python packages
+pip install -r requirements.txt
 
-if [-d "logs"]
-then
+# Check if the logs directory exists
+if [ -d "logs" ]; then
     echo "Log folder exists"
 else
     mkdir logs
-    touch logs/error.log logs.access.log
+    touch logs/error.log logs/access.log
 fi
 
+# Change permissions for the logs directory
 sudo chmod -R 777 logs
+
 echo "envsetup finishes"
