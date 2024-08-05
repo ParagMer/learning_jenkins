65009498488ed42f34ca336fa370df7814e310d7
Parag Mer <paragmerpm49@gmail.com>
2024-08-05 15:36:45 +0530

    jenkins pipilines updated

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,11 +1,16 @@
-sudo apt update
+sudo apt-get update -y
 
-sudo apt install openjdk-17-jre
- 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | 
+  sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+/etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
+sudo apt-get install jenkins
+
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
